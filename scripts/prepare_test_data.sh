1f1fe30ab4949b65a7de99144c74da11b3bb1c1d
Jorge Villarrubia <jorvil01@ucm.es>
2025-01-13 20:09:50 +0100

    Added script to include the rodinia kernels configured for the test automatically

---
+++ scripts/prepare_test_data.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# URL with the test-data to download
+URL="https://github.com/Jorgitou98/FAR_MIG_scheduler/releases/download/test-data/gpu-rodinia.tar.gz"
+
+# Path to the directory where the data will be stored
+DEST_DIR="../data/kernels"
+
+# Create the directory if it does not exist
+mkdir -p "$DEST_DIR"
+
+# Download the file from the URL using wget
+echo "Downloading file from $URL..."
+wget "$URL" -O "test_data.tar.gz"
+
+# Extract the file in the specified directory
+echo "Extracting file..."
+tar -xzvf test_data.tar.gz -C "$DEST_DIR"
+
+# Remove the .tar.gz file
+echo "Removing file .tar.gz..."
+rm test_data.tar.gz
+
+# Confirmation message
+echo "Process completed successfully!"
